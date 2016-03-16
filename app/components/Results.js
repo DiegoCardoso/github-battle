71ed439f9f6e5e958c4aeac5522cd37dd90ec997
Diego Cardoso <diegopc86@gmail.com>
2016-03-16 10:11:59 -0300

    Add the score result to the UI to show the battle's winner

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,12 +1,54 @@
 import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 
-const puke = (obj) => <pre>{JSON.stringify(obj, 2, ' ')}</pre>
+import UserDetails from './UserDetails';
+import UserDetailsWrapper from './UserDetailsWrapper';
+
+const StartOver = () => {
+    return (
+        <div className="col-sm-12" style={{"margin-top": 20}}>
+            <Link to="/playerOne">
+                <button type="button" className="btn btn-lg btn-danger">
+                    Start Over
+                </button>
+            </Link>
+        </div>
+    )
+}
 
 const Results = React.createClass({
     render() {
+        const winnerIndex = this.props.scores[0] > this.props.scores[1] ? 0 : 1;
+        const loserIndex = 1 - winnerIndex; // 1 - [0] = 1 ; 1 - [1] = 0
+        
+        if (this.props.isLoading) {
+            return (
+                <p>LOADING...</p>
+            )
+        }
+        
+        if (this.props.scores[0] === this.props.scores[1]) {
+            return (
+                <div className="jumbotron col-sm-12 text-center">
+                    <h1>It's a tie</h1>
+                    <StartOver />                    
+                </div>
+            )
+        }
+        
         return (
-            <div>
-                Results: {puke(this.props)}
+            <div className="jumbotron col-sm-12 text-center">
+                <h1>Results</h1>
+                <div className="col-sm-8 col-sm-offset-2">
+                    <UserDetailsWrapper header="Winner">
+                        <UserDetails score={this.props.scores[winnerIndex]} info={this.props.playersInfo[winnerIndex]} />
+                    </UserDetailsWrapper>
+                    
+                    <UserDetailsWrapper header="Loser">
+                        <UserDetails score={this.props.scores[loserIndex]} info={this.props.playersInfo[loserIndex]} />
+                    </UserDetailsWrapper>
+                </div>
+                <StartOver />
             </div>
         );
     }
